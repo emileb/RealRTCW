da9ce22757ba7902ec02b71f5ab31799d053b04e
Emile Belanger <emile.belanger@gmail.com>
2025-09-04 18:54:37 +0100

    Use correct bot library

--- mobile/Android.mk
+++ mobile/Android.mk
@@ -101,7 +101,7 @@ LOCAL_SRC_FILES =  $(PROJECT_FILES)
 
 
 LOCAL_LDLIBS := -lEGL -ldl -llog -lOpenSLES -lz -lGLESv1_CM
-LOCAL_STATIC_LIBRARIES := sigc libzip libpng logwritter  SDL2_net iortcw_botlib.sp
+LOCAL_STATIC_LIBRARIES := sigc libzip libpng logwritter  SDL2_net realrtcw_botlib.sp
 LOCAL_SHARED_LIBRARIES := touchcontrols SDL2  SDL2_mixer core_shared saffal
 
 
